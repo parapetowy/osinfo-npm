fbc7d5d37e1ee997fb4d10d5f0a1173bd53b35f9
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-08-22 15:30:21 +0200

    added colors

---
+++ modules/OSinfo.js
@@ -0,0 +1,24 @@
+var os = require('os');
+var colors = require('colors');
+var secToTime = require('../modules/secTotime');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:' .bgBlue , type);
+    console.log('Release:' .red , release);
+    console.log('CPU model:' .blue, cpu);
+    console.log('Uptime: ~' .green, secToTime.print(uptime));
+    console.log('User name:' .yellow, userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
+
+exports.print = getOSinfo;
